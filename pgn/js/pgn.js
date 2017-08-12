b3b389b5c3e720b414bc24aa93f39b81df382a2c
Jyrki Heikkinen <jyrki.heikkinen@thirdpresence.com>
2017-08-12 11:24:26 +0300

    renamed directories

--- pgn/js/pgn.js
+++ pgn/js/pgn.js
@@ -1,5 +1,6 @@
-function completeIndex (path, filename) {
-  setGamesTotal(path + filename)
+function completeIndex (baseURI, filename) {
+  var pgnPath = baseURI.replace(document.location.origin, '')
+  setGamesTotal(pgnPath + filename)
   addLinks2PgnReader()
 
   function setGamesTotal (fullname) {
@@ -14,12 +15,12 @@ function completeIndex (path, filename) {
   }
 
   function addLinks2PgnReader () {
-    var baseUrl = document.location.origin + '/html/pgn.html#pgn=$filename|title=$title'
+    var urlPath = document.location.origin + '/pgn/#pgn=$filename|title=$title'
 
     $('#games tr > td:last-of-type').click(function openPgnReader () {
       var link = $(this).parent().find('td:first-of-type a')
       if (link) {
-        var url = baseUrl.replace('$filename', path + link.attr('href'))
+        var url = urlPath.replace('$filename', pgnPath + link.attr('href'))
           .replace('$title', encodeURIComponent(link.text()))
         window.open(url)
       }
